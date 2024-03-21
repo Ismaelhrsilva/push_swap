ada6bc3a96a3d3fe534c57680b6b198a1fe92886
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2024-03-21 18:41:50 -0300

    começando4

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:28:56 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/03/21 18:33:27 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/03/21 18:40:55 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,11 +64,31 @@ static void get_list(t_stack **a, char *list_int)
 	list = ft_split(list_int, ' ');
 	while (list[i])
 	{
-		node = add_node(ft_atoi(&list[i][0]));
+		node = add_node(ft_atoi(list[i]));
 		ft_printf("%d\n", node->value);
 		if (*a == NULL)
 		{
-			ft_printf("Paaa 1\n");
+			add_back(a, node);
+		}
+		else 
+			add_back(a, node);
+		i++;
+	}
+	return ;
+}
+
+static void get_list_2(t_stack **a, char **list_int)
+{
+	int i;
+	t_stack	*node;
+
+	i = 0;
+	while (list_int[i] != NULL)
+	{
+		node = add_node(ft_atoi(list_int[i]));
+		ft_printf("%d\n", node->value);
+		if (*a == NULL)
+		{
 			add_back(a, node);
 		}
 		else 
@@ -95,6 +115,6 @@ int	main(int argc, char **argv)
 	else if (argc == 2)
 		get_list(&pushswap->a, argv[1]);
 	else
-		ft_printf("Aqui");
+		get_list_2(&pushswap->a, argv);
 	return (0);
 }
