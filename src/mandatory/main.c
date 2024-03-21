f78912c8dbea23fbccef4c3bc43a984f5ad05500
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2024-03-21 18:33:58 -0300

    começando3

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:28:56 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/03/21 17:39:06 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/03/21 18:33:27 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,35 +17,39 @@ static t_stack	*add_node(int value)
 	t_stack	*a;
 
 	a = malloc(1 * sizeof(t_stack));
-	if (a == 0)
-		return (0);
+	if (a == NULL)
+		return (NULL);
 	a->value = value;
 	a->next = NULL;
 	a->previous = NULL;
 	return (a);
 }
 
-static t_stack	*go_last_stack(t_stack *a)
+/*static t_stack	*go_last_stack(t_stack *a)
 {
 	if (!a)
 		return (0);
 	else
 	{
-		while (a->next != NULL)
+		while (a && a->next != NULL)
 			a = a->next;
 		return (a);
 	}
-}
+}*/
 
 static void	add_back(t_stack **a, t_stack *node)
 {
 	t_stack *temp;
 
+	if (node == NULL)
+		return  ;
 	if (*a == NULL)
 		*a = node;
 	else
 	{
-		temp = go_last_stack(*a);
+		temp = *a;
+		while (temp && temp->next != NULL)
+			temp = temp->next;
 		temp->next = node;
 	}
 }
@@ -60,9 +64,11 @@ static void get_list(t_stack **a, char *list_int)
 	list = ft_split(list_int, ' ');
 	while (list[i])
 	{
-		node = add_node(list[i][0]);
-		if (a == NULL)
+		node = add_node(ft_atoi(&list[i][0]));
+		ft_printf("%d\n", node->value);
+		if (*a == NULL)
 		{
+			ft_printf("Paaa 1\n");
 			add_back(a, node);
 		}
 		else 
@@ -72,14 +78,18 @@ static void get_list(t_stack **a, char *list_int)
 	return ;
 }
 
+static t_pushswap *init_pushswap(void)
+{
+	static t_pushswap pushswap;
 
+	return (&pushswap);
+}
 
 int	main(int argc, char **argv)
 {
-	t_pushswap	*pushswap;
+	static t_pushswap	*pushswap;
 
-	pushswap = NULL;
-	pushswap = malloc(1 * sizeof(t_pushswap));
+	pushswap = init_pushswap();
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
 	else if (argc == 2)
