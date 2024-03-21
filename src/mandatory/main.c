21243d006c6736d1993fe14a24d8257171ccff27
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2024-03-21 17:39:17 -0300

    começando2

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:28:56 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/03/21 17:01:51 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/03/21 17:39:06 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,23 @@ static t_stack	*add_node(int value)
 	t_stack	*a;
 
 	a = malloc(1 * sizeof(t_stack));
-	if (!a)
-		return (NULL);
+	if (a == 0)
+		return (0);
 	a->value = value;
 	a->next = NULL;
 	a->previous = NULL;
 	return (a);
 }
 
-static t_stack	*go_last_stack(t_stack **a)
+static t_stack	*go_last_stack(t_stack *a)
 {
-	t_stack *temp;
-
-	temp = *a;
-	if (a == NULL)
-		return (NULL);
+	if (!a)
+		return (0);
 	else
 	{
-		while (temp->next != NULL)
-			temp = temp->next;
-		return (temp);
+		while (a->next != NULL)
+			a = a->next;
+		return (a);
 	}
 }
 
@@ -44,14 +41,11 @@ static void	add_back(t_stack **a, t_stack *node)
 {
 	t_stack *temp;
 
-	if (a == NULL)
-	{
+	if (*a == NULL)
 		*a = node;
-		return ;
-	}
 	else
 	{
-		temp = go_last_stack(a);
+		temp = go_last_stack(*a);
 		temp->next = node;
 	}
 }
@@ -85,8 +79,6 @@ int	main(int argc, char **argv)
 	t_pushswap	*pushswap;
 
 	pushswap = NULL;
-	pushswap->a = NULL;
-	pushswap->b = NULL;
 	pushswap = malloc(1 * sizeof(t_pushswap));
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
