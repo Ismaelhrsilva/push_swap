3adb673754f4f6045b9321685b0e3673bf8a056e
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2024-03-23 17:20:57 -0300

    antes de travar

--- src/mandatory/stack_suport.c
+++ src/mandatory/stack_suport.c
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 16:59:21 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/03/23 17:15:53 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/03/23 17:19:55 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	sorted_stack(t_stack *stack)
 	t_list	*temp;
 	int	size;
 
-	if (stack)
-		return (0);
 	temp = stack->head;
+	if (!temp)
+		return (0);
 	size = stack->size - 1;
 	while (size > 0)
 	{
