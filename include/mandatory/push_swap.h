c11e7404a98d5c6cc3505a4b82376bdc974f075d
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2024-04-04 19:34:55 -0300

    quase

--- include/mandatory/push_swap.h
+++ include/mandatory/push_swap.h
@@ -6,108 +6,103 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:32:30 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/04/03 21:24:32 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/04/04 19:27:07 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H 
-# define PUSH_SWAP_H
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include "../lib/libft/libft.h"
-# include "../lib/printf/ft_printf.h"
+#include "../lib/libft/libft.h"
+#include "../lib/printf/ft_printf.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-# define NUMBER 3
+#define NUMBER 7
 
-typedef enum e_operations
-{
-	SA = 1 << 1,
-	SB = 1 << 2,
-	SS = 1 << 3,
-	PA = 1 << 4,
-	PB = 1 << 5,
-	RA = 1 << 6,
-	RB = 1 << 7,
-	RR = 1 << 8,
-	RRA = 1 << 9,
-	RRB = 1 << 10,
-	RRR = 1 << 11,
-}		t_operations;
+typedef enum e_operations {
+  SA = 1 << 1,
+  SB = 1 << 2,
+  SS = 1 << 3,
+  PA = 1 << 4,
+  PB = 1 << 5,
+  RA = 1 << 6,
+  RB = 1 << 7,
+  RR = 1 << 8,
+  RRA = 1 << 9,
+  RRB = 1 << 10,
+  RRR = 1 << 11,
+} t_operations;
 
-typedef	struct s_pos
-{
-	int		value;
-	long	pos;
-}	t_pos;
+typedef struct s_pos {
+  int value;
+  long pos;
+} t_pos;
 
-typedef struct s_aux
-{
-	int	medium;
-	int	big;
-	int	ra;
-	int	rb;
-	int	pa;
-	int	pb;
-}	t_aux;
+typedef struct s_aux {
+  int medium;
+  int big;
+  int ra;
+  int rb;
+  int pa;
+  int pb;
+} t_aux;
 
-typedef struct s_stack
-{
-	t_list	*head;
-	int	size;
-}	t_stack;
+typedef struct s_stack {
+  t_list *head;
+  int size;
+} t_stack;
 
-enum e_flags
-{
-	STACK_A,
-	STACK_B,
+enum e_flags {
+  STACK_A,
+  STACK_B,
 };
 
-typedef int	(*t_weight)(int a, int b);
+typedef int (*t_weight)(int a, int b);
 
 /*** Utils Functions ***/
-void	print_stack(t_stack *stack);
-void	get_list(t_stack *stack, char *list_int);
-void	get_list_2(t_stack *stack, char **list_int, int size);
-int		*get_number(char *nbr);
+void print_stack(t_stack *stack);
+void get_list(t_stack *stack, char *list_int);
+void get_list_2(t_stack *stack, char **list_int, int size);
+int *get_number(char *nbr);
 t_stack *init_stack(void);
 t_stack *init_stack_b(void);
 
 /*** Functions implementation of stack ***/
-void	push(t_stack *stack, void *content);
-void	*pop(t_stack *stack);
-void	swap(t_stack *stack);
-void	rotate(t_stack *stack);
-void	reverse_rotate(t_stack *stack);
+void push(t_stack *stack, void *content);
+void *pop(t_stack *stack);
+void swap(t_stack *stack);
+void rotate(t_stack *stack);
+void reverse_rotate(t_stack *stack);
 
 /*** Functions stack suport ***/
-int		sorted_stack(t_stack *stack);
-t_pos	pos_(t_weight weight, t_stack *stack);
-int		bigger(int a, int b);
-int		smaller(int a, int b);
-void	*see_stack(t_stack *stack);
+int sorted_stack(t_stack *stack);
+t_pos pos_(t_weight weight, t_stack *stack);
+int bigger(int a, int b);
+int smaller(int a, int b);
+void *see_stack(t_stack *stack);
 
 /*** Functions Sort less five ***/
-void	sort_3(t_stack *stack);
-void	sort_4(t_stack *stack, t_stack *stack_b);
-void	sort_5(t_stack *stack, t_stack *stack_b);
-//void	sort_handler(t_stack *stack, t_stack *stack_b, int flag, int size);
-void	sort_handler(t_stack *stack, t_stack *stack_b, int flag, int size);
-void	three_handler_a(t_stack *stack, t_stack *stack_b);
-void	three_handler_b(t_stack *stack, t_stack *stack_b);
+void sort_three(t_stack *stack);
+void sort_3(t_stack *stack);
+void sort_4(t_stack *stack, t_stack *stack_b);
+void sort_5(t_stack *stack, t_stack *stack_b);
+void sort_handler(t_stack *stack, t_stack *stack_b, int flag, int size);
+void three_handler_a(t_stack *stack, t_stack *stack_b);
+void three_handler_b(t_stack *stack, t_stack *stack_b);
 
 /*** Functions operations ***/
-void	operations(t_operations operations, t_stack *stack, t_stack *stack_b);
-void	loop(t_operations operations, long x, t_stack *stack, t_stack *stack_b);
-void	print_operations(t_stack *stack);
+void operations(t_operations operations, t_stack *stack, t_stack *stack_b);
+void loop(t_operations operations, long x, t_stack *stack, t_stack *stack_b);
+void print_operations(t_stack *stack);
 
 /*** Functions Algorithm ***/
-void	ft_sort(int size, t_stack *stack, t_stack *stack_b, int *count);
-void	ft_sort_b(int size, t_stack *stack, t_stack *stack_b, int *count);
+void ft_sort(int size, t_stack *stack, t_stack *stack_b, int *count);
+void ft_sort_b(int size, t_stack *stack, t_stack *stack_b, int *count);
 
 /*** Functions about aux big and medium ***/
-int		smaller_middler(t_stack *stack, int size);
-int		bigger_middler(t_stack *stack, int size);
-int		middler(t_stack *stack, int range, int size);
+int smaller_middler(t_stack *stack, int size);
+int bigger_middler(t_stack *stack, int size);
+int middler(t_stack *stack, int range, int size);
 
 #endif
--- src/mandatory/sort_less_5.c
+++ src/mandatory/sort_less_5.c
@@ -6,94 +6,79 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 18:28:26 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/04/03 21:23:25 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/04/04 19:17:21 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mandatory/push_swap.h"
 
-void	sort_3(t_stack *stack)
-{
-	t_pos	small;
-	t_pos	big;
+void sort_three(t_stack *stack) {
+  int big;
 
-	if (stack->size == 1)
-		return ;
-	else if (stack->size == 2 && *(int *)stack->head->next->content > *(int *)stack->head->content)
-		return (operations(SA, stack, NULL));
-	small = pos_(smaller, stack);
-	big = pos_(bigger, stack);
-	if (big.pos == 0)
-		operations(RA, stack, NULL);
-	else if (big.pos == 1)
-		operations(RRA, stack, NULL);
-	if (*(int *)see_stack(stack) != small.value)
-		operations(SA, stack, NULL);
+  big = bigger_middler(stack, stack->size);
+  if (*(int *)stack->head->content == big)
+    operations(RA, stack, NULL);
+  else if (*(int *)stack->head->next->content == big)
+    operations(RRA, stack, NULL);
+  if (*(int *)stack->head->content > *(int *)stack->head->next->content)
+    operations(SA, stack, NULL);
 }
 
-void	sort_4(t_stack *stack, t_stack *stack_b)
-{
-	t_pos	small;
+void sort_3(t_stack *stack) {
+  t_pos small;
+  t_pos big;
 
-	if (stack->size <= 3)
-		return (sort_3(stack));
-	small = pos_(smaller, stack);
-	if (small.pos == 1)
-		operations(RA, stack, NULL);
-	else if (small.pos == 2)
-		loop(RA, 2, stack, NULL);
-	else if (small.pos == 3)
-		operations(RRA, stack, NULL);
-	if (sorted_stack(stack))
-		return ;
-	operations(PB, stack, stack_b);
-	sort_3(stack);
-	operations(PA, stack, stack_b);
+  if (stack->size == 1)
+    return;
+  else if (stack->size == 2 &&
+           *(int *)stack->head->next->content > *(int *)stack->head->content)
+    return (operations(SA, stack, NULL));
+  small = pos_(smaller, stack);
+  big = pos_(bigger, stack);
+  if (big.pos == 0)
+    operations(RA, stack, NULL);
+  else if (big.pos == 1)
+    operations(RRA, stack, NULL);
+  if (*(int *)see_stack(stack) != small.value)
+    operations(SA, stack, NULL);
 }
 
-void	sort_5(t_stack *stack, t_stack *stack_b)
-{
-	t_pos	small;
+void sort_4(t_stack *stack, t_stack *stack_b) {
+  t_pos small;
 
-	if (stack->size <= 4)
-		return (sort_4(stack, stack_b));
-	small = pos_(smaller, stack);
-	if (small.pos == 1)
-		operations(RA, stack, NULL);
-	else if (small.pos == 2)
-		loop(RA, 2, stack, NULL);
-	else if (small.pos == 3)
-		loop(RRA, 2, stack, NULL);
-	else if (small.pos == 4)
-		operations(RRA, stack, NULL);
-	if (sorted_stack(stack))
-		return ;
-	operations(PB, stack, stack_b);
-	sort_4(stack, stack_b);
-	operations(PA, stack, stack_b);
+  if (stack->size <= 3)
+    return (sort_3(stack));
+  small = pos_(smaller, stack);
+  if (small.pos == 1)
+    operations(RA, stack, NULL);
+  else if (small.pos == 2)
+    loop(RA, 2, stack, NULL);
+  else if (small.pos == 3)
+    operations(RRA, stack, NULL);
+  if (sorted_stack(stack))
+    return;
+  operations(PB, stack, stack_b);
+  sort_3(stack);
+  operations(PA, stack, stack_b);
 }
 
-/*void	sort_handler(t_stack *stack, t_stack *stack_b, int flag, int size)
-{
-	if (size < 3)
-		return ;
-	if (flag == 0)
-	{
-		if (!sorted_stack(stack))
-			sort_5(stack, stack_b);
-		else
-			return ;
-	}
-	else
-	{
-		if (!sorted_stack(stack_b))
-			sort_5(stack_b, stack);
-		else
-		{
-			loop(PA, size, stack, stack_b);
-			return ;
-		}
-	}
-	sort_handler(stack, stack_b, 0, (size / 2) + (size % 2));
-	sort_handler(stack, stack_b,  1, (size / 2));
-}*/
+void sort_5(t_stack *stack, t_stack *stack_b) {
+  t_pos small;
+
+  if (stack->size <= 4)
+    return (sort_4(stack, stack_b));
+  small = pos_(smaller, stack);
+  if (small.pos == 1)
+    operations(RA, stack, NULL);
+  else if (small.pos == 2)
+    loop(RA, 2, stack, NULL);
+  else if (small.pos == 3)
+    loop(RRA, 2, stack, NULL);
+  else if (small.pos == 4)
+    operations(RRA, stack, NULL);
+  if (sorted_stack(stack))
+    return;
+  operations(PB, stack, stack_b);
+  sort_4(stack, stack_b);
+  operations(PA, stack, stack_b);
+}
