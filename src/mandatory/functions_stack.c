3815f8d8ef94bb5ffef633f325b78cf002815d08
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2024-04-04 20:03:35 -0300

    quase dando make

--- src/mandatory/functions_stack.c
+++ src/mandatory/functions_stack.c
@@ -6,80 +6,75 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:28:56 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/03/23 15:49:10 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/04/04 19:35:08 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mandatory/push_swap.h"
 
-void	push(t_stack *stack, void *content)
-{
-	t_list *node;
+void push(t_stack *stack, void *content) {
+  t_list *node;
 
-	node = ft_lstnew(content);
-	if (!node)
-		exit(EXIT_FAILURE);
-	ft_lstadd_front(&stack->head, node);
-	stack->head = node;
-	stack->size++;
+  node = ft_lstnew(content);
+  if (!node)
+    exit(EXIT_FAILURE);
+  ft_lstadd_front(&stack->head, node);
+  stack->head = node;
+  stack->size++;
 }
 
-void	*pop(t_stack *stack)
-{
-	t_list	*temp;
-	void	*value;
+void *pop(t_stack *stack) {
+  t_list *temp;
+  void *value;
 
-	if (stack == NULL)
-		return (NULL);
-	temp = stack->head;
-	value = temp->content;
-	stack->head = temp->next;
-	ft_lstdelone(temp, NULL);
-	stack->size--;
-	return (value);
+  if (stack == NULL)
+    return (NULL);
+  temp = stack->head;
+  value = temp->content;
+  stack->head = temp->next;
+  ft_lstdelone(temp, NULL);
+  stack->size--;
+  return (value);
 }
 
-void	swap(t_stack *stack)
-{
-	void	*a;
-	void	*b;
+void swap(t_stack *stack) {
+  void *a;
+  void *b;
 
-	if (stack->size < 2)
-		return ;
-	a = pop(stack);
-	b = pop(stack);
-	push(stack, a);
-	push(stack, b);
+  if (stack->size < 2)
+    return;
+  a = pop(stack);
+  b = pop(stack);
+  push(stack, a);
+  push(stack, b);
 }
 
-void	rotate(t_stack *stack)
-{
-	void	*value;
-	t_list	*node;
+void rotate(t_stack *stack) {
+  void *value;
+  t_list *node;
 
-	if (stack->size < 2)
-		return ;
-	value = pop(stack);
-	node = ft_lstnew(value);
-	if (!node)
-		exit(EXIT_FAILURE);
-	ft_lstadd_back(&stack->head, node);
-	stack->size++;
+  if (stack->size < 2)
+    return;
+  value = pop(stack);
+  node = ft_lstnew(value);
+  if (!node)
+    exit(EXIT_FAILURE);
+  ft_lstadd_back(&stack->head, node);
+  stack->size++;
 }
 
-void	reverse_rotate(t_stack *stack)
-{
-	t_list	*temp;
-	void	*value;
+void reverse_rotate(t_stack *stack) {
+  t_list *temp;
+  void *value;
 
-	if (stack->size < 2)
-		return ;
-	temp = stack->head;
-	while (temp->next->next)
-		temp = temp->next;
-	value = temp->next->content;
-	ft_lstdelone(temp, NULL);
-	temp->next = NULL;
-	push(stack, value);
-	stack->size--;
+  if (stack->size < 2)
+    return;
+  temp = stack->head;
+  while (temp->next->next)
+    temp = temp->next;
+  value = temp->next->content;
+  ft_lstdelone(temp, NULL);
+  temp->next = NULL;
+  push(stack, value);
+  stack->size--;
 }
