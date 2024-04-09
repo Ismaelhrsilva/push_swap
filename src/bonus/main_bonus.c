130d43fbd0c45561bc3899cdd7863c3389cd0dc8
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2024-04-09 19:18:41 -0300

    quase finalizando o bonus

--- src/bonus/main_bonus.c
+++ src/bonus/main_bonus.c
@@ -6,22 +6,11 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:28:56 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/04/09 19:13:54 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/04/09 19:17:24 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bonus/push_swap_bonus.h"
-
-/*static void	push_move(t_move *move, void *content)
-{
-	t_list	*node;
-
-	node = ft_lstnew(content);
-	if (!node)
-		exit(EXIT_FAILURE);
-	ft_lstadd_back(&move->head, node);
-	move->size++;
-}*/
 
 static int	read_from_stdin(t_stack *stack, t_stack *stack_b)
 {
@@ -46,25 +35,6 @@ static int	read_from_stdin(t_stack *stack, t_stack *stack_b)
 	return (result);
 }
 
-static void	print_stack(t_stack *stack)
-{
-	t_list	*node;
-
-	if (!stack)
-		return ;
-	node = stack->head;
-	ft_putchar('[');
-	while (node != NULL)
-	{
-		ft_printf("%d", (*(int *)node->content));
-		if (node->next != NULL)
-			ft_putstr(", ");
-		node = node->next;
-	}
-	ft_putchar(']');
-	ft_putchar('\n');
-}
-
 t_stack	*init_stack(void)
 {
 	static t_stack	stack;
@@ -79,22 +49,13 @@ t_stack	*init_stack_b(void)
 	return (&stack_b);
 }
 
-t_move	*init_move(void)
-{
-	static t_move	move;
-
-	return (&move);
-}
-
 int	main(int argc, char **argv)
 {
 	static t_stack	*stack;
 	static t_stack	*stack_b;
-	static t_move	*move;
 
 	stack = init_stack();
 	stack_b = init_stack_b();
-	move = init_move();
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		exit(EXIT_FAILURE);
 	else if (argc == 2)
@@ -108,8 +69,6 @@ int	main(int argc, char **argv)
 	}
 	if (read_from_stdin(stack, stack_b))
 	{
-		print_stack(stack);
-		print_stack(stack_b);
 		if (sorted_stack(stack) && !stack_b->head)
 			ft_printf("OK\n");
 		else
